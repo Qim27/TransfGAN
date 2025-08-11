e45e21c31bd3d7274a3f478e036ea282a9f68670
Qim27 <73268722+Qim27@users.noreply.github.com>
2025-08-11 21:35:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ import ResCNN
 import visionTransformer
 import EnhanceAndDecoder
 import train
-#import evaluation
-#import comparisonCode.传统算法2
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
+'''main函数/Main関数'''
 
-'''main函数'''
 
+imgPath = './test_imgs/'        #输入照片存储路径/画像の保存先
 
-imgPath = './test_imgs/'        #输入照片存储路径
-
-def test():
+def test():                    #テスト用関数
     lightList = inputImage.openFunction(imgPath + 'o1.JPG')     #返回分割后的图片结果
     darkList = inputImage.openFunction(imgPath + 'u1.JPG')
 
@@ -28,9 +25,6 @@ def test():
     discriminatorLight = visionTransformer.openFunction_discriminator(lightList,darkList,generateImg)    #输入进鉴别器进行判定
 
 
-
 if __name__ == "__main__":
     train.training().call()
     #evaluation.test().call()
-    #comparisonCode.传统算法2.mmmm()
-    #test()
