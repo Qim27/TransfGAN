9845453a6ae0a3e037ef6ff31bea93732f026849
Qim27 <73268722+Qim27@users.noreply.github.com>
2025-12-17 18:27:04 +0900

    Refactor train.py for improved structure and training

--- train.py
+++ train.py
@@ -10,13 +10,13 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
-'''训练用/トレーニング用'''
+'''トレーニング用'''
 
 
 class allModel(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        self.model_res = ResCNN.Rescnn()                                #读取模型/モデルの読み取り
+        self.model_res = ResCNN.Rescnn()                                #すべてのモデルを結合するために新たに定義したモデル
         self.model_res2 = ResCNN.Rescnn()
         #self.model_ViT = visionTransformer.visionTransformer(768)
         self.model_entance = EnhanceAndDecoder.enhanceBlock(512)
@@ -29,16 +29,16 @@ class allModel(tf.keras.Model):
         tensor_resDark = self.model_res2(inputList_darkS)
 
         #tensor_ViT = self.model_ViT(tf.cast(tf.concat([inputList_lightS,inputList_darkS],1),'float32'))
-        #Y = self.model_decoder(self.model_entance(tensor_resLight,tensor_resDark),tensor_ViT)
+        #モデルの各モジュールを組み合わせ，アブレーション実験の要件に対応
         Y = self.model_decoder_temp(self.model_entance(tensor_resLight,tensor_resDark))
         #Y = self.model_decoder_temp(tensor_resLight,tensor_resDark)
         return Y
     
 
 
-#超参数定义/ハイパーパラメータ
+#ハイパーパラメータの定義
 learnRate = 0.001
-numberEpochs = 20
+numberEpochs = 1000
 allNumber = 20
 batchSize_s = 5
 testImang_filerPath = './Dataset_Part1/'
@@ -48,7 +48,7 @@ modelSave_path = './model/'
 class training():
     def __init__(self):
         print('开始训练')
-        self.model = allModel()                                     #读取模型
+        self.model = allModel()                                     #モデルを読み込む
         self.discriminator = visionTransformer.Discriminator()
         #self.cosineDecay_lr = tf.keras.optimizers.schedules.CosineDecay(learnRate,3000)
         self.exponentialDecay_lr = tf.keras.optimizers.schedules.ExponentialDecay(learnRate,20,0.999)
@@ -60,14 +60,14 @@ class training():
             
 
 
-    def call(self):         #开始训练
+    def call(self):         #学習開始
         inputList_light_trainData,inputList_dark_trainData,inputList_normal_trainData, \
             inputList_light_testData,inputList_dark_testData,inputList_normal_testData = self.inputImage_all(testImang_filerPath)
 
         endTime,allTime,nowTime = [0,0,0]
         batchSum = int(allNumber / batchSize_s)
 
-
+        #以下は生成器のみを単独で学習するコード部分
         self.model.compile(optimizer=tf.keras.optimizers.Adam(self.exponentialDecay_lr),loss=[lossFunction_meanSquared(),lossFunction_ssim()],metrics=['Accuracy','binary_crossentropy'],loss_weights=[0.1,20])
         history = self.model.fit([inputList_light_trainData,inputList_dark_trainData],inputList_normal_trainData,
                                  batchSize_s,numberEpochs,validation_data=([inputList_light_testData,inputList_dark_testData],inputList_normal_testData),shuffle=False)
@@ -79,20 +79,20 @@ class training():
         inputImage.saveImage_tensorImg(outImage_train)
         inputImage.saveImage_tensorImg(outImage_test,'./outImages/test/')
         inputImage.saveImage(inputList_normal_trainData[:20],'./outImages/normal/')
-        evaluation.test().call()
+        evaluation.test().call()        #評価関数を統合して実行
         print('training is over')
         return
         
         
-        for i in range(numberEpochs):           #训练循环
+        for i in range(numberEpochs):           #学習ループ
             
             allTime = endTime - nowTime + allTime
-            print(f'====== epoch:{i+1}/{numberEpochs}. time spent:{(endTime-nowTime):.3f}, total time:{allTime:.3f}. ======')             #输出基本信息
+            print(f'====== epoch:{i+1}/{numberEpochs}. time spent:{(endTime-nowTime):.3f}, total time:{allTime:.3f}. ======')             #基本情報を出力
             nowTime = tf.timestamp()
             for j in range(batchSum):
                 self.train_step([inputList_light_trainData[5*j:5*(j+1)],inputList_dark_trainData[5*j:5*(j+1)]],inputList_normal_trainData[5*j:5*(j+1)])
                 
-            #验证部分
+            #検証部分
             generatedImage_validation = self.model([inputList_light_testData,inputList_dark_testData])
             GLoss = lossFunction_generator().call(inputList_normal_testData,generatedImage_validation,[0.1,5])
             DLoss = lossFunction_crossEntropy().call(self.discriminator(generatedImage_validation),self.discriminator(inputList_normal_testData))
@@ -105,15 +105,7 @@ class training():
             print('save error')
         else:
             print('model is saved')
-       
-        '''
-        #使用keras提供的方法训练模型
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(learnRate),loss=[lossFunction_meanSquared(),lossFunction_ssim()],
-                           metrics=['Accuracy','mean_squared_error'],loss_weights=[0.5,20])
-        
-        history = self.model.fit([inputList_light_trainData,inputList_dark_trainData],inputList_normal_trainData,
-                                 batchSize_s,numberEpochs,validation_data=([inputList_light_testData,inputList_dark_testData],inputList_normal_testData),shuffle=False)
-        '''    
+ 
         
         outImage_train = self.model([inputList_light_trainData[:20],inputList_dark_trainData[:20]])
         outImage_test = self.model([inputList_light_testData[:20],inputList_dark_testData[:20]])
@@ -127,20 +119,20 @@ class training():
 
 
 
-    def train_step(self,inputList,trueImage):              #定义训练每步的内容
+    def train_step(self,inputList,trueImage):              #各学習ステップの処理を定義
         genertedImage = self.model(inputList)
 
-        discriminatorLoss1 = self.discriminator.train_on_batch(trueImage,tf.ones(batchSize_s,'float32'))                        #先训练鉴别器
+        discriminatorLoss1 = self.discriminator.train_on_batch(trueImage,tf.ones(batchSize_s,'float32'))                        #先に識別器を学習
         discriminatorLoss2 = self.discriminator.train_on_batch(genertedImage,tf.zeros(batchSize_s,'float32'))
         discriminatorLoss = (discriminatorLoss1[0] + discriminatorLoss2[0]) / 2
 
-        generatorLoss = self.adversarialModel.train_on_batch(inputList,[trueImage,trueImage,tf.ones(batchSize_s,'float32')])    #后训练生成器
-        #输出结果
+        generatorLoss = self.adversarialModel.train_on_batch(inputList,[trueImage,trueImage,tf.ones(batchSize_s,'float32')])    #次に生成器を学習
+        #結果を出力
         print(f'Gloss: {generatorLoss[0]:.9f}, (MSLoss:{generatorLoss[1]:.4f}, ssimLoss:{generatorLoss[2]:.9f}, DLoss:{generatorLoss[3]:.7f}),Accuracy: {generatorLoss[4]:.9f}, Dloss: {discriminatorLoss:.7f} ')
         return 
 
     
-    def adversarial(self):      #训练生成器用模型
+    def adversarial(self):      #生成器学習用モデル
         inputList1 = tf.keras.layers.Input(shape=(256,256,3),batch_size=batchSize_s)
         inputList2 = tf.keras.layers.Input(shape=(256,256,3),batch_size=batchSize_s)
         generatorOut = self.model([inputList1,inputList2])
@@ -155,7 +147,7 @@ class training():
 
 
 
-    #读取数据集/データセットの読み取り
+    #データセットを読み込む
     def inputImage_all(self,testImang_filerPath,allNumber=allNumber):
         inputList_light = []
         inputList_dark = []
@@ -166,22 +158,22 @@ class training():
             imgPath_dark = testImang_filerPath + f'{i+1}/3.JPG'
             imgPath_normal = testImang_filerPath + f'{i+1}/8.JPG'
             
-            inputList_light.extend(inputImage.inImage(imgPath_light))           #读取图片
+            inputList_light.extend(inputImage.inImage(imgPath_light))           #画像を読み込む
             inputList_dark.extend(inputImage.inImage(imgPath_dark))
             inputList_normal.extend(inputImage.inImage(imgPath_normal))
 
-            if(len(inputList_light) > allNumber):                               #到数量就退出循环
+            if(len(inputList_light) > allNumber):                               #指定数に達したらループを終了
                 break
             
 
-        state = numpy.random.get_state()                #按相同顺序打乱三个数据集
+        state = numpy.random.get_state()                #3つのデータセットを同一順序でシャッフル
         numpy.random.shuffle(inputList_light)
         numpy.random.set_state(state)
         numpy.random.shuffle(inputList_dark)
         numpy.random.set_state(state)
         numpy.random.shuffle(inputList_normal)
 
-        allSize = len(inputList_light)                  #分成训练用和测试用数据集
+        allSize = len(inputList_light)                  #学習用とテスト用に分割
         trainSize = int(allSize * 0.9)
 
         inputList_light_trainData = numpy.array(inputList_light[ : trainSize],'float32')
@@ -227,10 +219,11 @@ class lossFunction_ssim(tf.keras.losses.Loss):
 
 
 
-class lossFunction_crossEntropy(tf.keras.losses.Loss):                          #交叉熵损失函数
+class lossFunction_crossEntropy(tf.keras.losses.Loss):                          #交差エントロピー損失関数
     def call(self,outImage,trueImage):
         #Y = -tf.reduce_sum(trueImage_discriminatorOutput * tf.math.log(tf.math.softmax(outImage_discriminatorOutput)),-1)
         #Y = -(tf.reduce_sum(trueImage * tf.math.log(outImage) + (1 - trueImage) * tf.math.log(1 - outImage)) / tf.cast(tf.reduce_sum(trueImage.shape),'float32'))
         Y1 = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(trueImage,outImage))
         return Y1
+
 
